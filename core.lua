3f0aff1bddcd50f0deb019660b27fc3b99333798
wowboxingonabudget <60112433+wowboxingonabudget@users.noreply.github.com>
2020-02-14 18:53:21 -0800

    Add files via upload

---
+++ core.lua
@@ -0,0 +1,13 @@
+local POWER_EventFrame = CreateFrame("Frame")
+POWER_EventFrame:RegisterEvent("UNIT_POWER_UPDATE")
+POWER_EventFrame:SetScript("OnEvent",
+	function(self, event, ...)
+		local unitPower = UnitPower("player" , 10)
+		--print(unitPower)
+		arg1, arg2 = ...
+		if arg2 == "ALTERNATE" then
+			if unitPower < 900 then 
+				PlaySound(8959)
+			end
+		end
+	end)
